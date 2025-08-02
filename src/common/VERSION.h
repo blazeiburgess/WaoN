af47a1d471890f13c0e253844d778a0da3c737d7
Blaze Burgess <blaze.i.burgess@gmail.com>
2025-08-02 15:06:56 -0400

    Update version to 0.11 and improve versioning utilities
    - Updated `VERSION.h` to reflect new version `0.11`.
    - Added detailed version components: `WAON_VERSION_MAJOR`, `WAON_VERSION_MINOR`, and `WAON_VERSION_PATCH`.
    - Introduced `WAON_BUILD_DATE` and `WAON_BUILD_TIME` macros for build metadata.
    - Defined `WAON_VERSION_NUMBER` for easier version comparisons.
    - Added `WAON_VERSION_FULL` macro combining version and build information.
    - Included license and documentation updates in `VERSION.h`.

--- src/common/VERSION.h
+++ src/common/VERSION.h
@@ -1,1 +1,33 @@
-#define WAON_VERSION "0.10"
+/* Version information for WaoN
+ * Copyright (C) 2024 WaoN Development Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _VERSION_H_
+#define _VERSION_H_
+
+/* Version string */
+#define WAON_VERSION "0.11"
+
+/* Version components */
+#define WAON_VERSION_MAJOR 0
+#define WAON_VERSION_MINOR 11
+#define WAON_VERSION_PATCH 0
+
+/* Build information */
+#define WAON_BUILD_DATE __DATE__
+#define WAON_BUILD_TIME __TIME__
+
+/* Version as integer for comparisons */
+#define WAON_VERSION_NUMBER (WAON_VERSION_MAJOR * 10000 + \
+                            WAON_VERSION_MINOR * 100 + \
+                            WAON_VERSION_PATCH)
+
+/* Get full version string with build info */
+#define WAON_VERSION_FULL WAON_VERSION " (built " WAON_BUILD_DATE " " WAON_BUILD_TIME ")"
+
+#endif /* !_VERSION_H_ */
